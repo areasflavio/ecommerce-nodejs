0d8ff0a400a9ae06d07b0c9d7d7ffa88ddf62a1e
Flávio Arêas <vflavio9@gmail.com>
2021-08-31 07:58:39 -0300

    chore: initial app structure

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import express from 'express'
+
+const server = express();
+
+server.get('/', (req, res) => {
+  return res.json({ hello: "World" });
+})
+
+server.listen(3333, () => {
+  console.log("🛒 Server is up on port 3333...");
+  
+})
