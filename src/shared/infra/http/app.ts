ae30607cfda496297285a4e6c5616af22635e6df
Flávio Arêas <vflavio9@gmail.com>
2021-09-01 08:15:31 -0300

    chore: create app errors and add cors

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,11 +1,35 @@
-import express from 'express';
+import 'reflect-metadata';
+
+import express, { NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+import 'express-async-errors';
 
 import '@shared/infra/typeorm';
+import AppError from '@shared/errors/AppError';
 
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+
 app.get('/', (req, res) => {
 	return res.json({ hello: 'World' });
+});
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+	if (err instanceof AppError) {
+		return response.status(err.statusCode).json({
+			status: 'error',
+			message: err.message,
+		});
+	}
+
+	console.log(err);
+
+	return response.status(500).json({
+		status: 'error',
+		message: 'Internal Server Error',
+	});
 });
 
 export default app;
