3aba21b3d21e57fde177ff69cf6f7e112226c209
Flávio Arêas <vflavio9@gmail.com>
2021-09-01 09:21:32 -0300

    chore: use dependency injection

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -4,8 +4,10 @@ import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import 'express-async-errors';
 
-import '@shared/infra/typeorm';
 import AppError from '@shared/errors/AppError';
+
+import '@shared/infra/typeorm';
+import '@shared/container';
 
 const app = express();
 
