fe337fb1aa3a41f6c41e8fd73f343b198ab7c944
Flávio Arêas <vflavio9@gmail.com>
2021-09-03 08:38:31 -0300

    feat: create Product entity

---
+++ src/shared/infra/typeorm/migrations/1630667941017-CreateProducts.ts
@@ -0,0 +1,49 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateProducts1630667941017 implements MigrationInterface {
+	public async up(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.createTable(
+			new Table({
+				name: 'products',
+				columns: [
+					{
+						name: 'id',
+						type: 'varchar',
+						isPrimary: true,
+						generationStrategy: 'uuid',
+						default: 'uuid_generate_v4()',
+					},
+					{
+						name: 'name',
+						type: 'varchar',
+					},
+					{
+						name: 'price',
+						type: 'decimal',
+						precision: 5,
+						scale: 2,
+					},
+					{
+						name: 'quantity',
+						type: 'int',
+						default: 0,
+					},
+					{
+						name: 'created_at',
+						type: 'timestamp with time zone',
+						default: 'now()',
+					},
+					{
+						name: 'updated_at',
+						type: 'timestamp with time zone',
+						default: 'now()',
+					},
+				],
+			})
+		);
+	}
+
+	public async down(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.dropTable('products');
+	}
+}
